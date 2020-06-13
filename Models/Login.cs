0307818cc1a812629b6495d5d0c20de0afbad29d
DAO-CHO <lndao.httt0117@student.ctuet.edu.vn>
2020-06-13 08:22:25 +0700

    bảng chức vụ

---
+++ Models/Login.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace shophoatuoi.Models
+{
+    public partial class Login
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
